ffc645ea542be3e0220685fdec813f423a94655e
Divakar21 <divakarganesh21@gmail.com>
2021-05-19 01:43:41 +0530

    Changes Done

--- src/test/java/org/test/TestRunner/TestRunner5.java
+++ src/test/java/org/test/TestRunner/TestRunner5.java
@@ -6,8 +6,8 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="C:\\Users\\user\\Downloads\\eclipse\\Divakar\\CucumberFramework\\src\\test\\resources\\Cucumber2.feature", 
-glue="org.test.steps", dryRun=false, monochrome=true,
+@CucumberOptions(features="C:\\Users\\Admin\\Desktop\\Divakar\\FrameworkProject2\\src\\test\\resources\\resources\\Features\\Cucumber2.feature", 
+glue="org.test.stepdefinition", dryRun=false, monochrome=true,
 plugin= {"pretty","html:target/cucumber-reports"})
 
 public class TestRunner5 {
